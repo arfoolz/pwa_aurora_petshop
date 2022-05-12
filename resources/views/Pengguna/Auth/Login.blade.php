7a2668c7d5e5227919d77b288facacbeccc55400
Arya <aryayudhareynardo@gmail.com>
2022-05-12 19:32:18 +0700

    Login Berhasil

--- resources/views/Pengguna/Auth/Login.blade.php
+++ resources/views/Pengguna/Auth/Login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+  
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,17 +11,24 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="{{ asset('StrapDashUser/assets/css/login.css')}}">
 </head>
+
 <body>
   <main>
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-6 login-section-wrapper">
+
           <div class="brand-wrapper">
             {{-- <img src="{{ asset('StrapDashUser/assets/images/logo.svg')}}" alt="logo" class="logo"> --}}
           </div>
+
           <div class="login-wrapper my-auto">
             <h1 class="login-title">Masuk</h1>
-            <form action="#!">
+
+            <form action="{{ route('postlogin_user') }}" method="post">
+
+              {{ csrf_field() }}
+
               <div class="form-group">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email" class="form-control" placeholder="email@example.com">
@@ -29,20 +37,27 @@
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password" class="form-control" placeholder="enter your passsword">
               </div>
-              <input  name="login" id="login" class="btn btn-block login-btn" type="button" value="Masuk">
+              <div class="mb-1">
+                <button type="submit" class="btn btn-block btn-outline-success">Masuk</button>
+              </div>
             </form>
+
             {{-- <a href="#!" class="forgot-password-link">Lupa password?</a> --}}
-            <p class="login-wrapper-footer-text">Belum Punya Akun? <a href="#!" class="text-reset">Daftar Disini</a></p>
+            <p class="login-wrapper-footer-text">Belum Punya Akun? <a href="/register" class="text-reset">Daftar Disini</a></p>
           </div>
         </div>
+
         <div class="col-sm-6 px-0 d-none d-sm-block">
           <img src="{{ asset('StrapDashUser/assets/images/login-cus.jpg')}}" alt="login image" class="login-img">
         </div>
+        
       </div>
     </div>
   </main>
+
   <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
 </body>
 </html>
